699af9b4afb60489f73d25569843fd50f6d63452
liu.renjun <liu.renjun@mydreamplus.com>
2018-06-25 15:29:13 +0800

    增加生成文件

--- src/app/main.go
+++ src/app/main.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"app/lib"
 	"io/ioutil"
-	"fmt"
+	"github.com/fatih/color"
 )
 
 func main() {
@@ -18,15 +18,32 @@ func main() {
 	requestNum := len(lib.AllRequest)
 	
 	i := 0
+	commentString := ""
 	for i < requestNum {
 		comment := lib.MakeComment(lib.AllRequest[i])
-		fmt.Println("/**")
+		commentString = joinComment(commentString, "/**")
 		for _, c := range comment {
-			fmt.Println(" *", c)
+			commentString = joinComment(commentString, " *"+c)
 		}
-		fmt.Println(" */")
-		fmt.Println("")
-		fmt.Println("")
+		
+		commentString = joinComment(commentString, " */")
+		commentString = joinComment(commentString, "\n\n")
 		i = i + 1
+	}
+	f, err := os.OpenFile("result.txt", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		color.Red(err.Error())
+	}
+	defer f.Close()
+	
+	f.WriteString(commentString)
+	color.Green("Done!")
+}
+
+func joinComment(source, newLine string) string {
+	if source != "" {
+		return source + "\n" + newLine
+	} else {
+		return newLine
 	}
 }
