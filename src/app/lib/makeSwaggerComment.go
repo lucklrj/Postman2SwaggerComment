c13ce0b7e6c5962989ef310b14c718fe33d63cba
lucklrj <sunny_lrj@yeah.net>
2019-04-05 01:35:11 +0800

    1.增加title部分 2.兼容更多格式

--- src/app/lib/makeSwaggerComment.go
+++ src/app/lib/makeSwaggerComment.go
@@ -1,8 +1,9 @@
 package lib
 
 import (
-	"strings"
+	"fmt"
 	"github.com/tidwall/gjson"
+	"strings"
 )
 
 type LineComent struct {
@@ -16,31 +17,65 @@ type SingleComment struct {
 	Body []string
 }
 
-func MakeComment(singeRequest Request) []string {
-	
+func MakeTile(host string, base_Path string, version string, title string, description string, contact string) []string {
 	comment := make([]string, 0)
 	blankIndex := 0
-	
+
+	comment = append(comment, blankRepeat(blankIndex)+"@SWG\\Swagger(")
+	blankIndex = blankIndex + 1
+
+	comment = append(comment, blankRepeat(blankIndex)+"schemes={\"http\",\"https\"},")
+
+	comment = append(comment, blankRepeat(blankIndex)+"host=\""+host+"\",")
+	comment = append(comment, blankRepeat(blankIndex)+"basePath=\""+base_Path+"\",")
+	comment = append(comment, blankRepeat(blankIndex)+"@SWG\\Info(")
+
+	blankIndex = blankIndex + 1
+
+	comment = append(comment, blankRepeat(blankIndex)+"version=\""+version+"\",")
+	comment = append(comment, blankRepeat(blankIndex)+"title=\""+title+"\",")
+	comment = append(comment, blankRepeat(blankIndex)+"description=\""+description+"\",")
+	comment = append(comment, blankRepeat(blankIndex)+"termsOfService=\"\",")
+	comment = append(comment, blankRepeat(blankIndex)+"@SWG\\Contact(")
+	blankIndex = blankIndex + 1
+	comment = append(comment, blankRepeat(blankIndex)+"email=\""+contact+"\"")
+
+	blankIndex = blankIndex - 1
+	comment = append(comment, blankRepeat(blankIndex)+")")
+
+	blankIndex = blankIndex - 1
+	comment = append(comment, blankRepeat(blankIndex)+")")
+	blankIndex = blankIndex - 1
+
+	comment = append(comment, blankRepeat(blankIndex)+")")
+
+	return comment
+}
+func MakeComment(singeRequest Request) []string {
+
+	comment := make([]string, 0)
+	blankIndex := 0
+
 	//请求方式
 	comment = append(comment, "@SWG\\"+singeRequest.Method+"(")
-	
+
 	blankIndex = blankIndex + 1
-	
+
 	//tags
 	tags := strings.Split(singeRequest.Name, "/")[0]
 	comment = append(comment, blankRepeat(blankIndex)+"tags={\""+tags+"\"},")
-	
+
 	//path
 	comment = append(comment, blankRepeat(blankIndex)+"path=\""+singeRequest.Path+"\",")
-	
+
 	//summary
 	summary := strings.Replace(singeRequest.Name, "/", "-", -1)
 	comment = append(comment, blankRepeat(blankIndex)+"summary=\""+summary+"\",")
-	
+
 	//Parameter
 	parameterIndex := 0
 	queryNum := len(singeRequest.Query)
-	
+
 	for parameterIndex < queryNum {
 		singeParameter := "@SWG\\Parameter(name =\"" + singeRequest.Query[parameterIndex].Key + "\", type=\"" + singeRequest.Query[parameterIndex].Type + "\", required=true, in=\"query\",description=\"" + singeRequest.Query[parameterIndex].Description + "\"),"
 		comment = append(comment, blankRepeat(blankIndex)+singeParameter)
@@ -49,31 +84,34 @@ func MakeComment(singeRequest Request) []string {
 	//Body
 	if singeRequest.Body.Mode == "raw" {
 		comment = append(comment, blankRepeat(blankIndex)+"@SWG\\Schema(")
-		
+
 		bodyComment := make([]LineComent, 0)
 		bodyComment = Json2Comemt(gjson.Parse(singeRequest.Body.Content.(string)), blankIndex, bodyComment)
-		
+
 		for _, singleBodyComment := range bodyComment {
 			comment = append(comment, blankRepeat(blankIndex+singleBodyComment.IndentNum)+singleBodyComment.Content)
 		}
 		comment = append(comment, blankRepeat(blankIndex)+"),")
-		
+
 	} else if singeRequest.Body.Mode == "formdata" || singeRequest.Body.Mode == "urlencoded" {
 		for _, singleBodyParameter := range singeRequest.Body.Content.([]Parameter) {
 			singeBodyParameter := "@SWG\\Parameter(name =\"" + singleBodyParameter.Key + "\", type=\"" + singleBodyParameter.Type + "\", required=true, in=\"body\",description=\"" + singleBodyParameter.Description + "\"),"
 			comment = append(comment, blankRepeat(blankIndex)+singeBodyParameter)
 		}
 	}
-	
+
 	//Response
 	comment = append(comment, blankRepeat(blankIndex)+"@SWG\\Response(")
 	blankIndex = blankIndex + 1
 	comment = append(comment, blankRepeat(blankIndex)+"response=\"200\",")
 	comment = append(comment, blankRepeat(blankIndex)+"description=\"接口响应\",")
 	comment = append(comment, blankRepeat(blankIndex)+"@SWG\\Schema(")
-	
+
 	responseComment := make([]LineComent, 0)
+
+	fmt.Println(singeRequest.Response)
 	responseComment = Json2Comemt(gjson.Parse(singeRequest.Response), blankIndex, responseComment)
+
 	for _, singleResponse := range responseComment {
 		comment = append(comment, blankRepeat(blankIndex+singleResponse.IndentNum)+singleResponse.Content)
 	}
@@ -96,7 +134,7 @@ func Json2Comemt(json gjson.Result, level int, responseComment []LineComent) []L
 		switch value.Type.String() {
 		case "Number":
 			line := LineComent{}
-			
+
 			thisValue := value.String()
 			thisType := ""
 			if strings.Contains(thisValue, ".") == true {
@@ -108,7 +146,7 @@ func Json2Comemt(json gjson.Result, level int, responseComment []LineComent) []L
 			line.IndentNum = level
 			responseComment = append(responseComment, line)
 			break
-		
+
 		case "String":
 			line := LineComent{}
 			thisValue := value.String()
@@ -122,7 +160,7 @@ func Json2Comemt(json gjson.Result, level int, responseComment []LineComent) []L
 			line.IndentNum = level
 			responseComment = append(responseComment, line)
 			break
-		
+
 		case "JSON":
 			if value.IsArray() == true { //返回数据为空时，则设置为空字符串
 				len := len(value.Array())
@@ -131,24 +169,47 @@ func Json2Comemt(json gjson.Result, level int, responseComment []LineComent) []L
 					line.Content = "@SWG\\Property( property=\"data\" , type=\"string\" , example=\"\",description=\"填写描述\"),"
 					line.IndentNum = level
 					responseComment = append(responseComment, line)
-					
+
 				} else {
 					value = value.Array()[0]
-					
+
 					lineStart := LineComent{}
-					lineStart.Content = "@SWG\\Property( property=\"" + key.String() + "\" ,type=\"object\","
+					lineStart.Content = "@SWG\\Property( property=\"" + key.String() + "\" ,type=\"array\","
 					lineStart.IndentNum = level
 					responseComment = append(responseComment, lineStart)
 					responseComment = Json2Comemt(value, level+1, responseComment)
-					
+
 					lineEnd := LineComent{}
 					lineEnd.Content = "),"
 					lineEnd.IndentNum = level
 					responseComment = append(responseComment, lineEnd)
 				}
+			} else {
+				lineStart := LineComent{}
+				lineStart.Content = "@SWG\\Property( property=\"" + key.String() + "\" ,type=\"object\","
+				lineStart.IndentNum = level
+				responseComment = append(responseComment, lineStart)
+				responseComment = Json2Comemt(value, level+1, responseComment)
+
+				lineEnd := LineComent{}
+				lineEnd.Content = "),"
+				lineEnd.IndentNum = level
+				responseComment = append(responseComment, lineEnd)
 			}
+		case "True", "False":
+			//case "False":
+			line := LineComent{}
+			thisValue := value.String()
+			thisType := "bool"
+
+			line.Content = "@SWG\\Property( property=\"" + key.String() + "\" , type=\"" + thisType + "\" , example=\"" + thisValue + "\",description=\"填写描述\"),"
+			line.IndentNum = level
+			responseComment = append(responseComment, line)
+			break
+		default:
+			fmt.Println(value.Type.String())
 		}
-		
+
 		return true
 	})
 	return responseComment
--- src/app/main.go
+++ src/app/main.go
@@ -11,6 +11,13 @@ import (
 var (
 	inputFile  = flag.String("source", "", "Postman 导出的json文件")
 	outputFile = flag.String("output", "", "产生的注释文件")
+
+	host        = flag.String("host", "Project host", "项目地址,例如：api.xx.com")
+	basePath    = flag.String("base_path", "/", "项目地址,例如 /")
+	title       = flag.String("title", "Project Api", "项目名称")
+	description = flag.String("description", "Project description", "项目描述")
+	version     = flag.String("version", "", "项目版本号")
+	contact     = flag.String("contact", "", "联系方式")
 )
 
 func init() {
@@ -20,14 +27,14 @@ func init() {
 		os.Exit(0)
 	}
 	if *outputFile == "" {
-		*outputFile = "result.txt"
+		*outputFile = "result.php"
 	}
 }
 func main() {
 	file, err := os.OpenFile(*inputFile, os.O_RDONLY, os.ModePerm)
 	lib.ErrorPut(err)
 	defer file.Close()
-	
+
 	fileContent, err := ioutil.ReadAll(file)
 	lib.FindRequest(string(fileContent), "")
 	lib.ErrorPut(err)
@@ -37,14 +44,20 @@ func main() {
 		os.Exit(0)
 	}
 	i := 0
-	commentString := ""
+	commentString := "<?php\n/**"
+	comment := lib.MakeTile(*host, *basePath, *version, *title, *description, *contact)
+	for _, c := range comment {
+		commentString = joinComment(commentString, " *"+c)
+	}
+	commentString = joinComment(commentString, " */\n\n")
+
 	for i < requestNum {
-		comment := lib.MakeComment(lib.AllRequest[i])
+		comment = lib.MakeComment(lib.AllRequest[i])
 		commentString = joinComment(commentString, "/**")
 		for _, c := range comment {
 			commentString = joinComment(commentString, " *"+c)
 		}
-		
+
 		commentString = joinComment(commentString, " */")
 		commentString = joinComment(commentString, "\n\n")
 		i = i + 1
